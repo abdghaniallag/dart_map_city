9d7e88569de6eef72de8e0b6a9a6f0814f807d5c
abdghaniallag <abdghaniallag@gmail.com>
2022-02-16 16:43:47 -0800

    remov sqlite3

---
+++ lib/model/model.dart
@@ -0,0 +1,11 @@
+class City {
+  int id;
+
+  String cityAscii;
+  int lat;
+  int lng;
+  int population;
+  int adminNameFk;
+  City(this.id, this.cityAscii, this.lat, this.lng, this.population,
+      this.adminNameFk);
+}
